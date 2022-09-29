1b091c5dfe6c0114b1f171a624a897ad4daff84c
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-09-29 17:15:29 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container"></div>
       Weather App
       <footer>
         This project was coded by{" "}
-        <a href="https://cozy-palmier-cefde3.netlify.app" target="_blank">
+        <a
+          href="https://cozy-palmier-cefde3.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Michelle Chan
         </a>
         and is{" "}
         <a
           href="https://github.com/michellechan40/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
