e77fd507bb77c2290d5f6f88f16dc8d1ea79e3af
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-09-29 10:07:44 -0600

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://cozy-palmier-cefde3.netlify.app" target="_blank">
+          Michelle Chan
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/michellechan40/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
