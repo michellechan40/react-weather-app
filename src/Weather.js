7508636c01281863259bb4a7de5b3773f3391775
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-09-29 18:40:50 -0600

    Worked on Weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
-          <div className="col-sm-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,15 +28,21 @@ export default function Weather() {
         <li>Wednesday 15:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
-        <div className="col-sm-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          23•C
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">23</span>
+              <span className="unit">•C</span>
+            </div>
+          </div>
         </div>
-        <div className="col-sm-6">
+        <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
             <li>Humidity: 77%</li>
