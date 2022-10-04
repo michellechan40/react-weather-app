9a486200faa9395e916104364563698ed77f0cb9
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-10-04 09:41:38 -0600

    Fixed weather css to get the icon and temperature positioned correctly

--- src/Weather.js
+++ src/Weather.js
@@ -12,8 +12,7 @@ export default function Weather(props) {
       ready: true,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
-      iconUrl:
-        response.data.weather[0].icon,
+      iconUrl: response.data.weather[0].icon,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,9 +17,9 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.iconUrl} />
+              <WeatherIcon code={props.data.iconUrl} size={52} />
             </div>
-            <div className="float-left">
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
